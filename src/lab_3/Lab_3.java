f0cf233afd3be046bc4305c13495d0ae90d9da50
Justin <Justin@DESKTOP-7LBTRBT>
2017-09-18 21:23:18 -0400

    setting up git/github

---
+++ src/lab_3/Lab_3.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab_3;
+import java.io.File;
+import java.io.FileNotFoundException;
+import static java.lang.System.in;
+import static java.lang.System.out;
+import java.util.List;
+import java.util.Scanner;
+/**
+ *
+ * @author Justin
+ */
+public class Lab_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        public List <BikePart> readFile(String fileName){
+            
+        }
+        
+        
+        
+//        try {
+//            System.out.print("Enter the file name with extension : ");
+//
+//            Scanner input = new Scanner(System.in);
+//
+//            File file = new File(input.nextLine());
+//
+//            input = new Scanner(file);
+//
+//
+////            while (input.hasNextLine()) {
+////                String line = input.nextLine();
+////                System.out.println(line + "\n");
+////            }
+//            input.close();
+//
+//        } catch (FileNotFoundException ex ) {
+//            out.println("File not found, please try a different file name ");
+//        }
+//        
+//        String[] bikeparts = readArray(file);
+//        
+////        System.out.println("Please enter how many lists you will entering.");
+////        Scanner in = new Scanner(System.in);
+////        int num = in.nextInt();
+////        
+////        BikePart[] bikePartArray = new BikePart [num];
+////        
+////        in.nextLine();
+////        System.out.println("Please enter the " + num + " lists");
+////        for(int i=0; i<num; i++){
+////            String s = in.next();
+////        
+////            String[] ss = s.split(",");
+////
+////            
+////        bikePartArray[i] = new BikePart(ss[0],Integer.parseInt(ss[1]),Double.parseDouble(ss[2]),Double.parseDouble(ss[3]),Boolean.parseBoolean(ss[4]),Integer.parseInt(ss[5]));
+////        }
+////        System.out.println("\nHere are all the parts less then 20$ \n");
+////        //System.out.println(bikePartArray);
+////        for(BikePart b: bikePartArray){
+////            if(b.getPrice() < 20)
+////                System.out.println(b);
+////        }
+//        
+//    }
+//    
+//    public static String[] readArray(String file) throws FileNotFoundException {
+//        int counter = 0;
+//        Scanner s1 = new Scanner(new File(file));
+//        while (s1.hasNextLine()){
+//            counter = counter + 1;
+//        }
+//        String[] bikeparts = new String[counter];
+//        Scanner s2 = new Scanner(new File(file));
+//        for (int i = 0; i < counter; i++){
+//            bikeparts[i] = s2.next();
+//        }
+//        return bikeparts;
+    }
+    
+    
+    //     seat,12345,15.00,10.00,true,100
+    //     Bell,23456,5.50,3.75,false,300
+    //     Wheel,98765,40.00,26.98,false,30
+}
+    
+    
+
